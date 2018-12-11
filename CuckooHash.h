e9fa062ad4d95831a0e3315f9f7c894a76812223
alexsoll <al.soluyanov@rambler.ru>
2018-12-11 22:26:53 +0300

    generation with Gaussian distribution. It is necessary to provide a task of k-independent function, comparison with other hash tables (the method of chains and one more.)

--- CuckooHash.h
+++ CuckooHash.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 //---------------------------------------------------------------------------------------------------------//
@@ -7,17 +8,18 @@ using namespace std;
 
 class THashTable{
 protected:
-	int HashFunc(int function, string k) {
-		
+	int HashFunc(int function, string k, int maxsize, int _a1, int _b1, int _a2, int _b2, int _p) {
 		int pos = 0;
 		switch (function) {
 		case 1:
 			for (int i = 0; i < k.length(); i++)
-				pos += ((k[i] * 20 + 57) % 32749) % 500;
+				pos += k[i];
+			pos = ((pos * _a1 + _b1) % _p) % maxsize;
 		
 		case 2:
 			for (int i = 0; i < k.length(); i++) {
-				pos += ((k[i] * 13 + 73) % 32749) % 500;
+				pos += k[i];
+			pos = ((pos * _a2 + _b2) % _p) % maxsize;
 			}
 		return pos;
 		}
@@ -30,24 +32,48 @@ protected:
 
 class TArrayHash : public THashTable {
 protected:
-	int maxsize, curr, DataCount;
+	int maxsize, curr, DataCount, a1, b1, p, a2, b2;
+	int num_of_rehash;
 	string **arr;
 	int pos[2];
 public:
 	TArrayHash(int size = 500) {
+		srand(time(NULL));
+		num_of_rehash = 0;
 		arr = new string*[2];
 		arr[0] = new string[size];
 		arr[1] = new string[size];
 		maxsize = size;
+		p = maxsize;
+		while (true) {
+			if (IsPrime(p) == true)
+				break;
+			else 
+				p++;	
+		}
+		a1 = rand() % p + 1;
+		b1 = rand() % p;
+		a2 = rand() % p + 1;
+		b2 = rand() % p;
 		for (int i = 0; i < maxsize; i++) {
-			arr[0][i] = " ";
-			arr[1][i] = " ";
+			arr[0][i] = "-";
+			arr[1][i] = "-";
 		}
 	}
 
+	bool IsPrime(int num) {
+		for (int i = 2; i <= sqrt(num); i++)
+			if (num%i == 0)
+				return false;
+		return true;
+	}
+
 	~TArrayHash() {
+		delete[] arr[1];
+		delete[] arr[2];
 		delete[] arr;
 	}
+
 
 	void clear() {
 		delete[] arr;
@@ -55,25 +81,50 @@ public:
 		arr[0] = new string[maxsize];
 		arr[1] = new string[maxsize];
 		for (int i = 0; i < maxsize; i++) {
-			arr[0][i] = " ";
-			arr[1][i] = " ";
+			arr[0][i] = "-";
+			arr[1][i] = "-";
 		}
 		DataCount = 0;
 		curr = 0;
 	}
 
+	void rehash() {
+		DataCount = 0;
+		//delete[] arr[1];
+		//delete[] arr[2];
+		delete[] arr;
 
-	bool Find(string k) {
-		curr = HashFunc(1,k);
+		arr = new string*[2];
+		arr[0] = new string[maxsize];
+		arr[1] = new string[maxsize];
+		a1 = rand() % p + 1;
+		b1 = rand() % p;
+		a2 = rand() % p + 1;
+		b2 = rand() % p;
+		for (int i = 0; i < maxsize; i++) {
+			arr[0][i] = "-";
+			arr[1][i] = "-";
+		}
+	}
+	int Find(string k) {
+		curr = HashFunc(1,k, maxsize, a1,b1,a2,b2,p);
 		if (arr[0][curr] == k || arr[0][curr] == k)
-			return true;
-		curr = HashFunc(2, k);
+			return curr;
+		curr = HashFunc(2, k, maxsize, a1, b1, a2, b2, p);
 		if (arr[1][curr] == k || arr[1][curr] == k)
-			return true;
-		return false;
+			return curr;
+		return -1;
 	}
 
-	void Place(string tr, int tableID, int cnt, int n) {
+
+	/* 
+	This variant of the Place function attempts to add 
+	an element to a particular hash table with the tableID index. 
+	This option does not allow you to add an item to another table 
+	when there is a free cell in it.
+	*/
+
+	/*void Place(string tr, int tableID, int cnt, int n) {  
 		if (cnt == n) {
 			cout << "Cycle present. Rehash. \n";
 			return;
@@ -90,6 +141,73 @@ public:
 		else {
 			arr[tableID][pos[tableID]] = tr;
 		}
+	}*/
+
+	/*
+	This implementation takes into account the possibility to 
+	choose between two tables if there is free space. If there are 
+	no places in any of them, a random one is selected and the element 
+	is added there. The extruded element tries to change position.
+	*/
+
+	bool Place(string tr, int cnt, int n) {
+		bool flag;
+		if (cnt == n) {
+			cout << "Cycle present. Rehash. \n";
+			//rehash();
+			num_of_rehash++;
+			return false;
+		}
+		pos[0] = (this->HashFunc(1, tr, maxsize, a1, b1, a2, b2, p));
+		pos[1] = (this->HashFunc(2, tr, maxsize, a1, b1, a2, b2, p));
+
+		if (arr[0][pos[0]] == tr || arr[1][pos[1]] == tr)
+			return true;
+		if (arr[0][pos[0]] != "-" && arr[1][pos[1]] == "-") {
+			arr[1][pos[1]] = tr;
+			DataCount++;
+			return true;
+		}
+		if (arr[1][pos[1]] != "-" && arr[0][pos[0]] == "-") {
+			arr[0][pos[0]] = tr;
+			DataCount++;
+			return true;
+		}
+		if (arr[1][pos[1]] != "-" && arr[0][pos[0]] != "-") {
+			int rand_position = rand() % 2;
+			string tmp = arr[rand_position][pos[rand_position]];
+			arr[rand_position][pos[rand_position]] = tr;
+			flag = Place(tmp, cnt + 1, n);
+			if (flag == false)
+				return false;
+		}
+		else {
+			int rand_position = rand() % 2;
+			arr[rand_position][pos[rand_position]] = tr;
+			DataCount++;
+		}
+	}
+	
+	string GetValueInFirstTable(int pos) {
+		return arr[0][pos];
+	}
+	string GetValueInSecondTable(int pos) {
+		return arr[1][pos];
+	}
+
+	int GetNumOfRehash() {
+		return num_of_rehash;
+	}
+
+	void remove(string st) {
+		int pos = Find(st);
+		if (pos == -1) {
+			return;
+		}
+		if (arr[0][curr] == st)
+			arr[0][curr] = "-";
+		else
+			arr[1][curr] = "-";
 	}
 
 	char* GetString(string str) {
@@ -103,6 +221,16 @@ public:
 
 		res[len] = '\0';
 		return res;
+	}
+
+
+	void printHash() {
+		cout << "--------------TABLE 1----------------TABLE2------------" << '\n';
+		for (int i = 0; i < maxsize; i++) {
+			string tmp = GetValueInFirstTable(i);
+			cout << i  << '\t' << GetString(arr[0][i]) << '\t' << GetString(arr[1][i]) << '\n';
+		}
+ 		cout << "--------------------------------------------------------";
 	}
 
 };
--- main.cpp
+++ main.cpp
@@ -1,23 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "CuckooHash.h"
+#include <time.h>
+#include <cstdlib>
+#include <random>
 using namespace std;
 
 
-TArrayHash Cuckoo(string *tr, int n) {
-	TArrayHash *hash;
-	hash = new TArrayHash();
+char* GetChar(string str) {
+	int len = str.length();
 
-	for (int i = 0, cnt = 0; i < n; i++, cnt = 0)
-		hash->Place(tr[i], 0, cnt, n);
+	char* res = new char[len + 1];
 
-	return *hash;
+	for (int i = 0; i < len; i++) {
+		res[i] = str[i];
+	}
+
+	res[len] = '\0';
+	return res;
 }
 
+void Cuckoo(string *tr, int size , int n, TArrayHash* &hash) {
+	bool flag;
+	for (int i = 0, cnt = 0; i < size; i++) {
+		flag = hash->Place(tr[i], cnt, n); 
+ 		if (flag == false) {
+			hash->rehash();
+			i = -1;
+		}
+	}
+}
+
+string randomStrGen(int length, std::normal_distribution<> &uid, std::mt19937 &gen) {
+	static string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	string result;
+	result.resize(length);
+
+	for (int i = 0; i < length; i++) {
+		result[i] = charset[uid(gen)];
+	}
+	//cout << GetChar(result) << " ";
+	return result;
+}
+
+
 int main() {
+
+	static string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	std::mt19937 gen(time(0));
+	std::normal_distribution<> dist(30, 7);
+	string result;
+	result.resize(4);
+
 	TArrayHash *hs;
-	hs = new TArrayHash();
+	int size = 400;
+	int num_of_data = 100;
+	hs = new TArrayHash(size);
 	string *t;
-	t = new string[2];
-	t[0] = "hello";
-	t[1] = "world";
-	*hs = Cuckoo(t, 2);
+	t = new string[num_of_data];
+	int n = floor(3 * log(2000));
+	for (int i = 0; i < num_of_data; i++) {
+		t[i] = randomStrGen(20, dist, gen);
+	}
+	Cuckoo(t,num_of_data,n, hs);
+	int rehash = hs->GetNumOfRehash();
+	cout << "Number of rehash is " << rehash << " " << '\n';
+	hs->printHash();
 }
